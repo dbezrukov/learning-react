c8b4c65911a96f1a081bd27952a745f8ed81a146
dbezrukov <dmitry.v.bezrukov@gmail.com>
2019-08-14 11:15:12 +0300

    updating posts ui

--- redux-thunk-blog/src/components/PostList.js
+++ redux-thunk-blog/src/components/PostList.js
@@ -7,11 +7,28 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    const posts = this.props.posts.map(post => 
-      <div key={post.title}>{post.title}</div>
-    )
-    return <div>{posts}</div>;
+    return (
+      <div className="ui relaxed divided list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
@@ -21,4 +38,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
   mapStateToProps, 
-  { fetchPosts })(PostList);
+  { fetchPosts }
+)(PostList);
